113b60e16814191620d9f5ab3dbb20444267cd69
Konstantin Chsherbakov <k.chsherbakov@alfabank.kz>
2019-05-17 17:24:30 +0600

    In progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
-'use strict';
-const fs = require('fs');
-
 function loadTimelineJson(callback) {
     let xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../content/timeline/content.json');
+    xobj.open('GET', './content/timeline/content.json');
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == '200') {
             callback(xobj.responseText);
@@ -14,7 +11,34 @@ function loadTimelineJson(callback) {
 }
 
 function initTimeline() {
-    loadTimelineJson(function (data) {
+    loadTimelineJson(async function (data) {
         let timelineData = JSON.parse(data);
+        let parentBlock = document.getElementById('timeline-feed').innerHTML;
+        let cards = '';
+
+        // Load templates
+        let positionCard = await fetch('./components/timeline/position.html')
+            .then(response => response.text());
+        for (let i = 0; i < timelineData.length; i++) {
+            let d = timelineData[i];
+            if (d["type"] == 'position') {
+                cards += positionCard.replace(/{{type}}/g, d['type'])
+                    .replace(/{{start_date}}/g,
+                        Date.now().toLocaleDateString('en-en', {
+                            month: 'short',
+                            year: 'numeric'
+                        }))
+                    .replace(/{{end_date}}/g, d['end_date'])
+                    .replace(/{{title}}/g, d['title'])
+                    .replace(/{{org_logo}}/g, d['org_logo'])
+                    .replace(/{{org_logo_alt_name}}/g, d['org_logo_alt_name'])
+                    .replace(/{{org_name}}/g, d['org_name'])
+                    .replace(/{{org_link}}/g, d['org_link'])
+                    .replace(/{{description}}/g, d['description'])
+            }
+        }
+
+        // Add content to the page
+        document.getElementById('timeline-feed').innerHTML = cards;
     });
 }
