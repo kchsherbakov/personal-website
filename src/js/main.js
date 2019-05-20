3e0ccad55e9a03a65eb30de42adaf78ba36d2638
Konstantin Chsherbakov <k.chsherbakov@alfabank.kz>
2019-05-20 16:30:51 +0600

    Switched from components to local templates (<template>). Modified source code to work with inner template elements to reduce network communication.

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,123 @@
-function loadTimelineJson(callback) {
-    let xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', './content/timeline/content.json');
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == '200') {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
+const IMAGE_PATH_PREFIX = './img/';
+const ENUM_DATE_TYPE_YEAR = 0;
+const ENUM_DATE_TYPE_MONTH = 1;
+const ENUM_DATE_TYPE_MONTH_AND_YEAR = 2;
+
+function formatDate(date, type, locale) {
+    let dt = new Date(date);
+
+    if (type === ENUM_DATE_TYPE_YEAR) {
+        return dt.toLocaleDateString(locale, {
+            year: 'numeric'
+        });
+    } else if (type === ENUM_DATE_TYPE_MONTH) {
+        return dt.toLocaleDateString(locale, {
+            year: 'numeric'
+        });
+    } else if (type === ENUM_DATE_TYPE_MONTH_AND_YEAR) {
+        return dt.toLocaleDateString(locale, {
+            month: 'short',
+            year: 'numeric'
+        });
+    }
 }
 
-function initTimeline() {
-    loadTimelineJson(async function (data) {
-        let timelineData = JSON.parse(data);
-        let parentBlock = document.getElementById('timeline-feed').innerHTML;
-        let cards = '';
+function calcDateDiff(startDate, endDate, returnInterval) {
+    let sd = new Date(startDate);
+    let ed = new Date(endDate);
+    let diffDate = new Date(ed.getTime() - sd.getTime());
 
-        // Load templates
-        let positionCard = await fetch('./components/timeline/position.html')
-            .then(response => response.text());
-        for (let i = 0; i < timelineData.length; i++) {
-            let d = timelineData[i];
-            if (d["type"] == 'position') {
-                cards += positionCard.replace(/{{type}}/g, d['type'])
-                    .replace(/{{start_date}}/g,
-                        Date.now().toLocaleDateString('en-en', {
-                            month: 'short',
-                            year: 'numeric'
-                        }))
-                    .replace(/{{end_date}}/g, d['end_date'])
-                    .replace(/{{title}}/g, d['title'])
-                    .replace(/{{org_logo}}/g, d['org_logo'])
-                    .replace(/{{org_logo_alt_name}}/g, d['org_logo_alt_name'])
-                    .replace(/{{org_name}}/g, d['org_name'])
-                    .replace(/{{org_link}}/g, d['org_link'])
-                    .replace(/{{description}}/g, d['description'])
-            }
+    if (returnInterval === ENUM_DATE_TYPE_YEAR) {
+        // Calc year difference
+        return diffDate.getFullYear() - 1970;
+    } else if (returnInterval === ENUM_DATE_TYPE_MONTH) {
+        // Includes last month to the count
+        return diffDate.getMonth() + 1;
+    }
+}
+
+async function initTimeline() {
+    // Debug only settings
+    let fetchHeaders = new Headers();
+    fetchHeaders.append('pragma', 'no-cache');
+    fetchHeaders.append('cache-control', 'no-cache');
+
+    let fetchInit = {
+        method: 'GET',
+        headers: fetchHeaders,
+    };
+
+    let jsonContent = await fetch('./content/timeline/content.json', fetchInit)
+        .then(response => response.text());
+    let timelineData = JSON.parse(jsonContent);
+    let parentBlock = document.getElementById('timeline-feed');
+
+    // Templates
+    let cardTagTemplate = document.getElementById('card-tag-template');
+    let positionCardTemplate = document.getElementById('position-card-template');
+    let educationCardTemplate = document.getElementById('education-card-template');
+
+    for (let i = 0; i < timelineData.length; i++) {
+        let cardTemplateContent;
+        let d = timelineData[i];
+
+        if (d['type'] === 'position') {
+            cardTemplateContent = document.importNode(positionCardTemplate.content, true)
+                .querySelector(('div.timeline__item'));
+        } else if (d['type'] === 'education') {
+            cardTemplateContent = document.importNode(educationCardTemplate.content, true)
+                .querySelector(('div.timeline__item'));
         }
 
-        // Add content to the page
-        document.getElementById('timeline-feed').innerHTML = cards;
-    });
+        // Preformatting
+        if (d['end_date'] === '') {
+            d['end_date'] = Date.now();
+        }
+
+        if (d['date_show_type'] === ENUM_DATE_TYPE_MONTH_AND_YEAR) {
+            let years = calcDateDiff(d['start_date'], d['end_date'], ENUM_DATE_TYPE_YEAR);
+            let months = calcDateDiff(d['start_date'], d['end_date'], ENUM_DATE_TYPE_MONTH);
+            if (years >= 1) {
+                // Language formats
+                d['date_diff'] = years > 1 ? `${years} years` : `${years} year`;
+                d['date_diff'] += months > 1 ? `, ${months} months` : `, ${months} month`;
+            } else {
+                d['date_diff'] = months > 1 ? `${months} months` : `${months} month`;
+            }
+        } else if (d['date_show_type'] === 'y') {
+            let years = calcDateDiff(d['start_date'], d['end_date'], ENUM_DATE_TYPE_YEAR);
+            d['date_diff'] = years > 1 ? `${years} years` : `${years} year`;
+        } else {
+            let months = calcDateDiff(d['start_date'], d['end_date'], ENUM_DATE_TYPE_MONTH);
+            d['date_diff'] = d['date_diff'] = months > 1 ? `${months} months` : `${months} month`;
+        }
+
+        // Replace template data
+        cardTemplateContent.innerHTML = cardTemplateContent.innerHTML.replace(/{{type}}/g, d['type'])
+            .replace(/{{start_date}}/g,
+                formatDate(d['start_date'], d['date_show_type'], 'en-en'))
+            .replace(/{{end_date}}/g,
+                formatDate(d['end_date'], d['date_show_type'], 'en-en'))
+            .replace(/{{date_diff}}/g, d['date_diff'])
+            .replace(/{{title}}/g, d['title'])
+            .replace(/{{org_logo}}/g, IMAGE_PATH_PREFIX + d['org_logo'])
+            .replace(/{{org_logo_alt_name}}/g, d['org_logo_alt_name'])
+            .replace(/{{org_name}}/g, d['org_name'])
+            .replace(/{{org_link}}/g, d['org_link'])
+            .replace(/{{description}}/g, d['description']);
+
+        // Add tags
+        let tagsContainer = cardTemplateContent.querySelector('div.timeline-item__tags-container');
+        for (let j = 0; j < d['tags'].length; j++) {
+            let tag = d['tags'][j];
+            let content = document.importNode(cardTagTemplate.content, true);
+            let span = content.querySelector('span');
+            span.textContent = tag;
+
+            tagsContainer.appendChild(span);
+        }
+
+        // Append card
+        parentBlock.appendChild(cardTemplateContent);
+    }
 }
