513c5edf5a691f3d9673430e508250b532ceea04
Konstantin Chsherbakov <k.chsherbakov@outlook.com>
2021-05-31 18:39:29 +0600

    feature: react initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                TEST
+            </div>
+        )
+    }
+}
+
+export default App;
