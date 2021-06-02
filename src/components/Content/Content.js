e4c0485353cf73732621ffdef83bcdb823cb7b11
Konstantin Chsherbakov <k.chsherbakov@outlook.com>
2021-06-02 14:15:35 +0600

    feature: added parallax

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {HelloSection} from "./HelloSection";
+import {Parallax} from "react-scroll-parallax";
 
 class Content extends Component {
     render() {
@@ -7,10 +8,19 @@ class Content extends Component {
             <div className="content">
                 <div className="content__container">
                     <HelloSection {...this.props}/>
+                    <ParallaxImage/>
                 </div>
             </div>
         )
     }
+}
+
+const ParallaxImage = () => {
+    return (
+        <Parallax className="image-section-parallax" y={[-20, 20]} disabled={true}>
+            <div className="image-section-parallax__bgimage"/>
+        </Parallax>
+    )
 }
 
 export {Content}
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n'
 import {BrowserRouter} from 'react-router-dom';
+import {ParallaxProvider} from "react-scroll-parallax";
 
 ReactDOM.render(
     <BrowserRouter>
         <Suspense fallback="Temp loading...">
-            <I18nextProvider i18n={i18n}>
-                <App/>
-            </I18nextProvider>
+            <ParallaxProvider>
+                <I18nextProvider i18n={i18n}>
+                    <App/>
+                </I18nextProvider>
+            </ParallaxProvider>
         </Suspense>
     </BrowserRouter>,
     document.getElementById('app'));
