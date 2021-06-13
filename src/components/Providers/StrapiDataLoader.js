1f866b79e7189c2ae28b755eed5ad1909cc39f26
Konstantin Chsherbakov <k.chsherbakov@outlook.com>
2021-06-13 20:12:49 +0600

    feature: added error popup

--- src/components/Providers/StrapiDataLoader.js
+++ src/components/Providers/StrapiDataLoader.js
@@ -1,17 +1,23 @@
 import React from "react";
 import {StrapiContext} from "./StrapiProvider";
 import LoaderWrapper from "../LoaderWrapper";
+import ErrorWrapper from "../ErrorWrapper";
 
 export function StrapiDataLoader() {
     return (
         <StrapiContext.Consumer>
             {value => {
-                if (!value.isLoading)
-                    return;
+                if (value.isLoading) {
+                    return (
+                        <LoaderWrapper/>
+                    )
+                }
 
-                return (
-                    <LoaderWrapper/>
-                )
+                if (value.errorLoading !== null) {
+                    return (
+                        <ErrorWrapper/>
+                    )
+                }
             }}
         </StrapiContext.Consumer>
     )
